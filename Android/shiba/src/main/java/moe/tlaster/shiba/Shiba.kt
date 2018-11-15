ebe2239c4424868bda933ee7b9bc1e60c21bf90f
Tlaster <tlaster@outlook.com>
2018-11-15 19:10:45 +0800

    [Android] Update to LiquidCore to enable ES6 support

--- Android/shiba/src/main/java/moe/tlaster/shiba/Shiba.kt
+++ Android/shiba/src/main/java/moe/tlaster/shiba/Shiba.kt
@@ -1,12 +1,11 @@
 package moe.tlaster.shiba
 
 import android.util.ArrayMap
-import com.eclipsesource.v8.*
-import com.eclipsesource.v8.utils.V8ObjectUtils
 import moe.tlaster.shiba.commonProperty.GridProperty
 import moe.tlaster.shiba.commonProperty.ICommonProperty
 import moe.tlaster.shiba.extensionExecutor.BindingExecutor
 import moe.tlaster.shiba.mapper.*
+import org.liquidplayer.javascript.JSContext
 
 internal typealias NativeView = android.view.View
 internal typealias ShibaView = moe.tlaster.shiba.View
@@ -35,7 +34,7 @@ interface IArrayConversion : ITypeConversion {
 }
 
 class DefaultConverterExecutor : IConverterExecutor {
-    private val runtime = V8.createV8Runtime()
+    private val runtime = JSContext()
 
     private val conversions = ArrayList<ITypeConversion>()
 
@@ -44,40 +43,33 @@ class DefaultConverterExecutor : IConverterExecutor {
     }
 
     public fun addConverter(converter: String) {
-        runtime.executeScript(converter)
+        runtime.evaluateScript(converter)
     }
 
     override fun execute(name: String, parameters: Array<Any?>): Any? {
-        val obj = runtime.getObject(name)
-        if (obj is V8Function) {
-            val v8Parameters = V8Array(runtime)
-            parameters.forEach {
-                when (it) {
-                    is Double -> v8Parameters.push(it)
-                    is Int -> v8Parameters.push(it)
-                    is String -> v8Parameters.push(it)
-                    else -> {
-                        when (it) {
-                            null -> v8Parameters.pushNull()
-                            is List<*> -> {
-                                v8Parameters.push(V8ObjectUtils.toV8Array(runtime, it))
-                            }
-                            else -> {
-                                val converter = conversions.firstOrNull { conversion -> conversion.objectType == it.javaClass }
-                                when (converter) {
-                                    is IObjectConversion -> v8Parameters.push(V8ObjectUtils.toV8Object(runtime, converter.convert(it)))
-                                    is IArrayConversion -> v8Parameters.push(V8ObjectUtils.toV8Array(runtime, converter.convert(it)))
-                                    else -> v8Parameters.pushUndefined()
-                                }
-                            }
-                        }
+
+        val obj = runtime.property(name).toFunction()
+        if (obj != null) {
+            val result = obj.call(null, parameters.map { parameter ->
+                if (parameter == null) {
+                    return@map parameter
+                }
+                val converter = conversions.firstOrNull { conversion -> conversion.objectType == parameter.javaClass }
+                if (converter != null) {
+                    return@map when (converter) {
+                        is IObjectConversion -> converter.convert(parameter)
+                        is IArrayConversion -> converter.convert(parameter)
+                        else -> throw IllegalArgumentException()
                     }
                 }
+                return@map parameter
+            })
+            when {
+                result.isString -> return result.toString()
+                result.isBoolean -> return result.toBoolean()
+                result.isNumber -> return result.toNumber()
+                result.isArray -> return result.toJSArray()
             }
-
-            val result = obj.call(runtime, v8Parameters)
-            v8Parameters.release()
-            return result
         }
         return null
     }
--- Android/shiba/src/test/java/moe/tlaster/shiba/ExampleUnitTest.kt
+++ Android/shiba/src/test/java/moe/tlaster/shiba/ExampleUnitTest.kt
@@ -1,9 +1,5 @@
 package moe.tlaster.shiba
 
-import moe.tlaster.shiba.parser.ShibaParser
-import moe.tlaster.shiba.parser.ShibaParserWrapper
-import org.antlr.v4.runtime.tree.ParseTree
-import org.junit.Assert
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -17,11 +13,6 @@ class ExampleUnitTest {
     @Test
     @Throws(Exception::class)
     fun test() {
-        val list = "dataContext.dsadsa".split('.')
-        val a = list.indexOfFirst { it == "dsadsa" }
-        val c = list.drop(a)
-        val tree = ShibaParserWrapper().parse("stack { text -> awesome(reverse(\$bind UWPText)) text { color = \"#ffff00\" text = \"fdsafdsaf\" size = 20 } input -> \$bind UWPText }")
-        assert(tree != null)
     }
 
 }
